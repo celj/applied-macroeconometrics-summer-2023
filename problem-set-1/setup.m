29785748851bbc006b8c63cca1fd43f1aa58d1f7
Carlos Lezama <carloselezamaj@gmail.com>
2023-06-25 12:19:37 -0600

    add: problem set files

---
+++ problem-set-1/setup.m
@@ -0,0 +1,9 @@
+clc;
+clear all;
+close all;
+
+set(groot, 'DefaultAxesTickLabelInterpreter', 'latex');
+set(groot, 'DefaultTextInterpreter', 'latex');
+
+set(0,  'DefaultAxesFontSize', 16);
+set(0, 'DefaultLineLineWidth', 2);
---
+++ problem-set-1/src.m
@@ -0,0 +1,7 @@
+% Problem Set 1
+% Applied Macroeconometrics
+% ITAM
+% Summer 2023
+% Written by Carlos Lezama
+
+setup;
