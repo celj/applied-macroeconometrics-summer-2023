e84fc09310a4123839e4ba8d44928e8d61b83c72
Carlos Lezama <carloselezamaj@gmail.com>
2023-07-10 21:21:59 -0600

    update: improve efficiency

--- problem-set-3/src/valfun2.m
+++ problem-set-3/src/valfun2.m
@@ -5,7 +5,7 @@ global vlast beta delta alpha k0 kt phi theta
 k = x(1);
 h = x(2);
 g = interp1(k0,vlast,k,'linear');
-kk = kt ^ alpha * h ^ (1 - alpha) - k + (1 - delta) * kt;
+kk = 1.75 * kt ^ alpha * h ^ (1 - alpha) - k + (1 - delta) * kt; % add technology A_t = 1.75 to improve efficiency and convergence
 
 if kk <= .001
     val = log(.001) + phi * log(1 - h) + beta * g + (kk - .001);
